87977b3a0e2a4504b6bd814b3de0265724a3b731
Nuwan Sudusinghe <52767317+ArcReactive@users.noreply.github.com>
2020-07-29 15:28:10 +0530

    Code for sales and cost in Dashbord

--- ADBMSpro01/DashboardForm.cs
+++ ADBMSpro01/DashboardForm.cs
@@ -15,8 +15,13 @@ namespace ADBMSpro01
 {
     public partial class DashboardForm : Form
     {
-        
-        
+        public static SqlConnection myCon = null;
+        SqlDataReader DR;
+
+        float[] salesValue = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        float[] costValue = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+
         public DashboardForm()
         {
             InitializeComponent();
@@ -24,7 +29,94 @@ namespace ADBMSpro01
 
         private void DashboardForm_Load(object sender, EventArgs e)
         {
-            
+
+            //SALES and COST chart.
+
+            //connection.
+            DBconnection dbcon = new DBconnection();
+            myCon = dbcon.setCon();
+
+            //set sales.
+            using (myCon)
+            {
+                //read store procedure.
+                SqlCommand cmd = new SqlCommand("GetSalesTotalByMonth", myCon);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                DR = cmd.ExecuteReader();
+                int i = 0;
+                using (DR)
+                {
+                    while (DR.Read())
+                    {
+                        float value = (float)DR.GetDouble(0);
+                        salesValue[i] = value;
+                        i++;
+                    }
+                }
+                DR = null;
+            }
+
+            //set cost.
+            myCon = dbcon.setCon();
+
+            using (myCon)
+            {
+                //read store procedure.
+                SqlCommand cmd1 = new SqlCommand("GetCostTotalByMonth", myCon);
+
+                cmd1.CommandType = CommandType.StoredProcedure;
+
+                DR = cmd1.ExecuteReader();
+                int i = 0;
+                using (DR)
+                {
+                    while (DR.Read())
+                    {
+                        float value = (float)DR.GetDouble(0);
+                        costValue[i] = value;
+                        i++;
+                    }
+                }
+                DR = null;
+            }
+
+            //add two series to chart.
+            SalesChart.Series = new LiveCharts.SeriesCollection
+            {
+
+                    new LineSeries
+                    {
+                        Title = "Sales",
+                        Values = new ChartValues<float> {
+                            salesValue[0], salesValue[1], salesValue[2], salesValue[3], salesValue[4], salesValue[5], salesValue[6], salesValue[7], salesValue[8], salesValue[9], salesValue[10], salesValue[11]
+                        }
+
+                    },
+                    new LineSeries
+                    {
+                        Title = "Cost",
+                        Values = new ChartValues<double> {
+                            costValue[0], costValue[1], costValue[2], costValue[3], costValue[4], costValue[5], costValue[6], costValue[7], costValue[8], costValue[9], costValue[10], costValue[11]
+                        }
+                    },
+                };
+            //set the X axis.
+            SalesChart.AxisX.Add(new Axis
+            {
+                Title = "Month",
+                Labels = new[] { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" }
+            });
+
+            //set the Y axis.
+            SalesChart.AxisY.Add(new Axis
+            {
+                Title = "Price",
+                LabelFormatter = value => value.ToString("C")
+            });
+
+            SalesChart.LegendLocation = LegendLocation.Right;
 
 
 

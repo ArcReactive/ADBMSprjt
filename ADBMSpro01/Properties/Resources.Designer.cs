a09016273238b1f8c69bf315e5d758a6fdf24ef0
Nuwan Sudusinghe <52767317+ArcReactive@users.noreply.github.com>
2020-08-04 14:59:10 +0530

    update btn

--- ADBMSpro01/Properties/Resources.Designer.cs
+++ ADBMSpro01/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace ADBMSpro01.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap activateBtn {
+            get {
+                object obj = ResourceManager.GetObject("activateBtn", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap activeBtn {
             get {
                 object obj = ResourceManager.GetObject("activeBtn", resourceCulture);
@@ -96,6 +106,16 @@ namespace ADBMSpro01.Properties {
         internal static System.Drawing.Bitmap close_icon {
             get {
                 object obj = ResourceManager.GetObject("close icon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap deactivateBtn {
+            get {
+                object obj = ResourceManager.GetObject("deactivateBtn", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
